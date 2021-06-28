9a046fc6e545e4bbde0a114efd462d2093eddfe7
skchandon2 <67494675+skchandon2@users.noreply.github.com>
2021-06-28 14:13:34 -0400

    creating an empty asp.net core 3.1 project

---
+++ secure-approve/secure-approve/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace secure_approve.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
